0ebe23dbc35d44617e6e5a743adb1f170d1c6fdc
23ashutosh3 <60299983+23ashutosh3@users.noreply.github.com>
2020-04-15 12:18:08 +0530

    Update q3.js

--- q3.js
+++ q3.js
@@ -1,5 +1,16 @@
-function strContains(char)
+String.prototype.strContains=function (char)
 {
-  
+  var str=this;
+  for(var i=0;i<str.length;i++)
+  {
+    if(str.charAt(i)==char)
+    {
+      return true;
+    }
+    
+      
+  }
+  return false;
 }
-"I am a string".strContains('@'); 
+
+ console.log("I am a string".strContains("@"));
